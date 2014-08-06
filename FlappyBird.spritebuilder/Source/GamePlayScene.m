9f4b38113405921cf283ad1ca9461ef5362580df
ZaidHanania <zaid.hanania@gmail.com>
2014-08-06 08:36:23 -0400

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -7,6 +7,8 @@
 - (void)initialize
 {
     // your code here
+    character = (Character*)[CCBReader load:@"Character"];
+    [physicsNode addChild: character];
 }
 
 -(void)update:(CCTime)delta
@@ -15,5 +17,10 @@
 }
 
 // put new methods here
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event{
+    // called every time th eplayer touches the screen
+    [character flap];
+}
+
 
 @end
